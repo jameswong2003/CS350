f3dc3c4f3439f97fcc4edb9cba6f6dbd333dd23a
James <wong.james2003@gmail.com>
2024-02-04 11:47:52 -0500

    copying git from gitlab

--- bank/bank.go
+++ bank/bank.go
@@ -16,11 +16,7 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 
 // Bank represents a simple banking system
 type Bank struct {
-<<<<<<< HEAD
-	bankLock *sync.RWMutex
-=======
 	bankLock *sync.Mutex
->>>>>>> upstream/main
 	accounts map[int]*Account
 }
 
@@ -31,11 +27,7 @@ type Account struct {
 
 // initializes a new bank
 func BankInit() *Bank {
-<<<<<<< HEAD
-	b := Bank{&sync.RWMutex{}, make(map[int]*Account)}
-=======
 	b := Bank{&sync.Mutex{}, make(map[int]*Account)}
->>>>>>> upstream/main
 	return &b
 }
 
@@ -52,14 +44,10 @@ func (b *Bank) logInsufficientBalanceEvent(accountID int) {
 }
 
 func (b *Bank) sendEmailTo(accountID int) {
-<<<<<<< HEAD
-	// dummy lol
-=======
 	// Dummy function
 	// hello
 	// marker
 	// eraser
->>>>>>> upstream/main
 }
 
 // creates a new account with the given account ID
@@ -69,25 +57,15 @@ func (b *Bank) CreateAccount(accountID int) {
 
 // deposit a given amount to the specified account
 func (b *Bank) Deposit(accountID, amount int) {
-<<<<<<< HEAD
-	b.bankLock.RLock()
-	account := b.accounts[accountID]
-	b.bankLock.RUnlock()
-=======
 	b.bankLock.Lock()
 	account := b.accounts[accountID]
 	b.bankLock.Unlock()
->>>>>>> upstream/main
 
 	account.lock.Lock()
 	DPrintf("[ACQUIRED LOCK][DEPOSIT] for account %d\n", accountID)
 
 	newBalance := account.balance + amount
 	account.balance = newBalance
-<<<<<<< HEAD
-	// b.accounts[accountID] = account
-=======
->>>>>>> upstream/main
 	DPrintf("Deposited %d into account %d. New balance: %d\n", amount, accountID, newBalance)
 
 	b.accounts[accountID].lock.Unlock()
@@ -96,15 +74,9 @@ func (b *Bank) Deposit(accountID, amount int) {
 
 // withdraw the given amount from the given account id
 func (b *Bank) Withdraw(accountID, amount int) bool {
-<<<<<<< HEAD
-	b.bankLock.RLock()
-	account := b.accounts[accountID]
-	b.bankLock.RUnlock()
-=======
 	b.bankLock.Lock()
 	account := b.accounts[accountID]
 	b.bankLock.Unlock()
->>>>>>> upstream/main
 
 	account.lock.Lock()
 	DPrintf("[ACQUIRED LOCK][WITHDRAW] for account %d\n", accountID)
@@ -112,10 +84,6 @@ func (b *Bank) Withdraw(accountID, amount int) bool {
 	if account.balance >= amount {
 		newBalance := account.balance - amount
 		account.balance = newBalance
-<<<<<<< HEAD
-		// b.accounts[accountID] = &account
-=======
->>>>>>> upstream/main
 		DPrintf("Withdrawn %d from account %d. New balance: %d\n", amount, accountID, newBalance)
 		account.lock.Unlock()
 		DPrintf("[RELEASED LOCK][WITHDRAW] for account %d\n", accountID)
@@ -134,17 +102,10 @@ func (b *Bank) Withdraw(accountID, amount int) bool {
 
 // transfer amount from sender to receiver
 func (b *Bank) Transfer(sender int, receiver int, amount int, allowOverdraw bool) bool {
-<<<<<<< HEAD
-	b.bankLock.RLock()
-	senderAccount := b.accounts[sender]
-	receiverAccount := b.accounts[receiver]
-	b.bankLock.RUnlock()
-=======
 	b.bankLock.Lock()
 	senderAccount := b.accounts[sender]
 	receiverAccount := b.accounts[receiver]
 	b.bankLock.Unlock()
->>>>>>> upstream/main
 
 	senderAccount.lock.Lock()
 	receiverAccount.lock.Lock()
@@ -152,11 +113,7 @@ func (b *Bank) Transfer(sender int, receiver int, amount int, allowOverdraw bool
 	// if the sender has enough balance,
 	// or if overdraws are allowed
 	success := false
-<<<<<<< HEAD
-	if senderAccount.balance > amount || !allowOverdraw {
-=======
 	if senderAccount.balance >= amount || allowOverdraw {
->>>>>>> upstream/main
 		senderAccount.balance -= amount
 		receiverAccount.balance += amount
 		success = true
@@ -167,34 +124,16 @@ func (b *Bank) Transfer(sender int, receiver int, amount int, allowOverdraw bool
 }
 
 func (b *Bank) DepositAndCompare(accountId int, amount int, compareThreshold int) bool {
-<<<<<<< HEAD
-	b.bankLock.RLock()
-	account := b.accounts[accountId]
-	b.bankLock.RUnlock()
-=======
 	b.bankLock.Lock()
 	account := b.accounts[accountId]
 	b.bankLock.Unlock()
->>>>>>> upstream/main
 
 	account.lock.Lock()
 	defer account.lock.Unlock()
 
 	var compareResult bool
-<<<<<<< HEAD
-
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	go func() {
-		b.Deposit(accountId, amount)
-		compareResult = b.GetBalance(accountId) >= compareThreshold
-		wg.Done()
-	}()
-	wg.Wait()
-=======
 	b.Deposit(accountId, amount)
 	compareResult = b.GetBalance(accountId) >= compareThreshold
->>>>>>> upstream/main
 
 	// return compared result
 	return compareResult
