2046fe78cb2f2f1b8b8e06a3e4fa9f78d5910a92
James <wong.james2003@gmail.com>
2024-02-17 16:28:09 -0500

    worked on rpc.go to create necessary definitions and checks for coordinator

--- mr/coordinator.go
+++ mr/coordinator.go
@@ -6,20 +6,59 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
+	"time"
+)
+
+const WorkerDieTime = 10 * time.Second
+
+const (
+	MapPeriod = iota + 1
+	ReducePeriod
+	AllDone
 )
 
 type Coordinator struct {
 	// Your definitions here.
-
+	mutex     *sync.Mutex
+	taskQueue []*Task
+	index     int // Index of current free task
+	status    int32
+	NReduce   int32
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
 // an example RPC handler.
 // the RPC argument and reply types are defined in rpc.go.
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
+func (c *Coordinator) GetTask(req *TaskRequest, res *TaskResponse) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if c.status == AllDone {
+		res.ErrCode = ErrAllDone
+		return nil
+	}
+
+	n := len(c.taskQueue)
+	for i := 0; i < n; i++ {
+		t := c.taskQueue[c.index]
+		c.index = (c.index + 1) % n
+
+		if t.Status == StatusReady {
+			t.Status = StatusSent
+			res.Task = *t
+			res.ErrCode = ErrSuccess
+			go checkTask(c, t.TaskId, t.TaskType)
+			return nil
+		} else if t.status == StatusSent {
+			hasWaiting = true
+		}
+	}
+
+	if hasWaiting {
+
+	}
 }
 
 // mr-main/mrcoordinator.go calls Done() periodically to find out
